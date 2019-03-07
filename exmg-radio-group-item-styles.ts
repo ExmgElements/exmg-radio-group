3788b0c640ed1b20a2160fb2c89306a5bf1721c4
uladzislau <uladzislau.zayats@amsterdam-standard.pl>
2019-03-07 12:24:54 +0100

    PTWO-302 Radio group and item type form element

--- exmg-radio-group-item-styles.ts
+++ exmg-radio-group-item-styles.ts
@@ -21,38 +21,10 @@ export const exmgRadioGroupItemStyles = css`
       flex-direction: column;
       justify-content: center;
   }
-  .label.title {
+  .label > .title {
       text-transform: uppercase;
       color: #156fd8;
       font-weight: 500;
   }
 `;
-export const exmgRadioGroupItemStyles1 = css`
-  :host(:focus) {
-    outline: none;
-  }
-  .item {
-    border: 1px solid #dadada;
-    border-radius: 5px;
-    display: inline-block;
-    font-family: 'Roboto', 'Noto', sans-serif;
-    padding: 7px;
-  }
-  .item.checked {
-    border: 3px solid #156fd8;
-    padding: 5px;
-  }
-  .label {
-    /**
-      --paper-radio-button-label-spacing | Spacing between the label and the button | 10px
-      This spacing will cause that child block will be out of bounds. We have to apply negative margin on it,
-      and increase this margin 2 times to make it visually look nice
-     */
-    margin-right: calc(var(--paper-radio-button-label-spacing, 10px) * 2);
-  }
-  .title {
-    text-transform: uppercase;
-    color: #156fd8;
-    font-weight: 500;
-  }
-`;
+
